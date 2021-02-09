2df93c2977ccf19b2e4f954fec738b0ce8880feb
Bruno Baring <brunobaring@gmail.com>
2021-02-09 19:30:11 -0300

    Fixed DATABASE_URL environment variable key

--- emotion/config.py
+++ emotion/config.py
@@ -6,22 +6,26 @@ class BaseConfig(object):
     # BCRYPT_LOG_ROUNDS = 13
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('FILIN_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     UPLOAD_PATH = 'emotion/uploads'
+
+
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration."""
     # BCRYPT_LOG_ROUNDS = 4
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('FILIN_DATABASE_URL') + 'dev'
+
+
 
 class StagingConfig(BaseConfig):
     """Testing configuration."""
     # BCRYPT_LOG_ROUNDS = 4
     # PRESERVE_CONTEXT_ON_EXCEPTION = False
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('FILIN_DATABASE_URL') + 'stage'
+
+
 
 class ProductionConfig(BaseConfig):
     """Production configuration."""
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('FILIN_DATABASE_URL') + 'prod'
